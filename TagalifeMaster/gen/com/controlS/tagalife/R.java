5704587d2961283f1d84cd2065196e303cba3530
Sylvain <Sylvain@10.12.160.178>
2012-11-14 15:49:39 +0100

    menu & styles de texte

--- TagalifeMaster/gen/com/controlS/tagalife/R.java
+++ TagalifeMaster/gen/com/controlS/tagalife/R.java
@@ -10,31 +10,50 @@ package com.controlS.tagalife;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f040000;
+    }
+    public static final class dimen {
+        public static final int menu_dim=0x7f050000;
+    }
     public static final class drawable {
         public static final int background_hdpi=0x7f020000;
         public static final int ic_action_search=0x7f020001;
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int menu_apropos=0x7f090001;
+        public static final int menu_options=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f080000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int app_title=0x7f040001;
-        public static final int appels=0x7f040006;
-        public static final int dons=0x7f040007;
-        public static final int localiser=0x7f040004;
-        public static final int marche_suivre=0x7f040005;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_main=0x7f040003;
+        /**  nom de l'application 
+         */
+        public static final int app_name=0x7f060000;
+        /**  titre de l'application 
+         */
+        public static final int app_title=0x7f060001;
+        public static final int appels=0x7f060007;
+        public static final int dons=0x7f060008;
+        /**  nom des boutons 
+         */
+        public static final int localiser=0x7f060005;
+        public static final int marche_suivre=0x7f060006;
+        public static final int menu_apropos=0x7f060004;
+        /**  nom des menus 
+         */
+        public static final int menu_options=0x7f060003;
+        /**  page de l'application 
+         */
+        public static final int title_activity_main=0x7f060002;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f070000;
+        public static final int menuText=0x7f070001;
     }
 }
