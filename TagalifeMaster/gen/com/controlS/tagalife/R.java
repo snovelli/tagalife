2ee6ccc0a44051d7e128527d54bbaafde9e50ef2
Sylvain <Sylvain@10.12.160.178>
2012-11-14 14:18:11 +0100

    creation du background de l'application

--- TagalifeMaster/gen/com/controlS/tagalife/R.java
+++ TagalifeMaster/gen/com/controlS/tagalife/R.java
@@ -11,8 +11,9 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int background_hdpi=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
         public static final int menu_settings=0x7f070000;
@@ -25,7 +26,11 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
+        public static final int app_title=0x7f040001;
+        public static final int appels=0x7f040006;
+        public static final int dons=0x7f040007;
+        public static final int localiser=0x7f040004;
+        public static final int marche_suivre=0x7f040005;
         public static final int menu_settings=0x7f040002;
         public static final int title_activity_main=0x7f040003;
     }
