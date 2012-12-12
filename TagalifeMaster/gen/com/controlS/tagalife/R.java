c8a11cb2fc69ab27c4d3809c63736c4c77c8253b
famroune <famroune@NEO-L109.global.ad>
2012-12-12 12:16:41 +0100

    commit 12:17

--- TagalifeMaster/gen/com/controlS/tagalife/R.java
+++ TagalifeMaster/gen/com/controlS/tagalife/R.java
@@ -22,31 +22,44 @@ public final class R {
     }
     public static final class drawable {
         public static final int background_hdpi=0x7f020000;
-        public static final int effect_location=0x7f020001;
-        public static final int effect_note=0x7f020002;
-        public static final int ic_action_search=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int location_icon_clicked=0x7f020005;
-        public static final int location_icon_device=0x7f020006;
-        public static final int telephone_icon_device=0x7f020007;
-        public static final int wordpad_icon_clicked=0x7f020008;
-        public static final int wordpad_icon_device=0x7f020009;
+        public static final int effect_appel=0x7f020001;
+        public static final int effect_don=0x7f020002;
+        public static final int effect_location=0x7f020003;
+        public static final int effect_note=0x7f020004;
+        public static final int ic_action_search=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int location_icon_clicked=0x7f020007;
+        public static final int location_icon_device=0x7f020008;
+        public static final int telephone_icon_device=0x7f020009;
+        public static final int wordpad_icon_clicked=0x7f02000a;
+        public static final int wordpad_icon_device=0x7f02000b;
     }
     public static final class id {
-        public static final int imageButtonLocation=0x7f090000;
-        public static final int imageButtonNote=0x7f090001;
-        public static final int imageButtonPhone=0x7f090002;
-        public static final int menu_apropos=0x7f090004;
-        public static final int menu_options=0x7f090003;
-        public static final int menu_settings=0x7f090005;
+        public static final int imageButtonDon=0x7f090007;
+        public static final int imageButtonLocation=0x7f090004;
+        public static final int imageButtonNote=0x7f090005;
+        public static final int imageButtonPhone=0x7f090006;
+        public static final int menu_apropos=0x7f09000a;
+        public static final int menu_options=0x7f090009;
+        public static final int menu_settings=0x7f090008;
+        public static final int radioState=0x7f090000;
+        public static final int radioStateHigh=0x7f090003;
+        public static final int radioStateLow=0x7f090001;
+        public static final int radioStateMedium=0x7f090002;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_marche_as=0x7f030001;
+        public static final int activity_appel=0x7f030000;
+        public static final int activity_don=0x7f030001;
+        public static final int activity_location=0x7f030002;
+        public static final int activity_main=0x7f030003;
+        public static final int activity_marche_as=0x7f030004;
     }
     public static final class menu {
-        public static final int activity_main=0x7f080000;
-        public static final int activity_marche_as=0x7f080001;
+        public static final int activity_appel=0x7f080000;
+        public static final int activity_don=0x7f080001;
+        public static final int activity_main=0x7f080002;
+        public static final int activity_marche_as=0x7f080003;
+        public static final int activity_tag=0x7f080004;
     }
     public static final class string {
         /**  nom de l'application 
@@ -67,6 +80,8 @@ public final class R {
          */
         public static final int menu_options=0x7f060003;
         public static final int menu_settings=0x7f06000a;
+        public static final int title_activity_appel=0x7f06000c;
+        public static final int title_activity_don=0x7f06000d;
         /**  page de l'application 
          */
         public static final int title_activity_main=0x7f060002;
