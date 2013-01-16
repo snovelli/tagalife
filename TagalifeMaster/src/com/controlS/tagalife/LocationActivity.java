e4d03b11f6cbd0dfddc55cc9c64b9cfebe8c7fda
snovelli <novelli.sylvain@gmail.com>
2013-01-16 19:32:55 +0100

    XML generation 1st step

--- TagalifeMaster/src/com/controlS/tagalife/LocationActivity.java
+++ TagalifeMaster/src/com/controlS/tagalife/LocationActivity.java
@@ -2,30 +2,32 @@ package com.controlS.tagalife;
 
 
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import org.xmlpull.v1.XmlSerializer;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.util.Xml;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class LocationActivity extends Activity implements OnClickListener, LocationListener, OnSeekBarChangeListener {
 
@@ -52,7 +54,7 @@ public class LocationActivity extends Activity implements OnClickListener, Locat
 		findViewById(R.id.imageButtonValider).setOnClickListener(this);
 		
 		lManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-		getProvider();
+		createXMLDocument();
 		
 	}
 	
@@ -78,22 +80,83 @@ public class LocationActivity extends Activity implements OnClickListener, Locat
 		}
 	}
 	
-	private void createXMLDocument() throws ParserConfigurationException {
-		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+	private void createXMLDocument() {
 		
-		Document document = documentBuilder.newDocument();
+		File tagXML = new File(Environment.getExternalStorageDirectory() + "/tagXML.xml");
+		try {
+			tagXML.createNewFile();
+		} catch (IOException ioe) {
+			Log.e("IOException", "exception in createNewFile() method");
+		}
 		
-		Element rootElement = document.createElement("Tag");
-		Element sexeElement = document.createElement("sexe");
-//		sexeElement.setAttribute("sexe", R.id.textViewEtat);
-		Element ageElement 	= document.createElement("age");
-		Element etatElement = document.createElement("etat");
+		FileOutputStream fileos = null;
+		try {
+			fileos = new FileOutputStream(tagXML);
+		} catch (FileNotFoundException fnfe) {
+			Log.e("FileNotFoundException", "can't create FileOutputStream");
+		}
 		
-		rootElement.appendChild(sexeElement).appendChild(ageElement).appendChild(etatElement);
-		
-		
-		
+		XmlSerializer serializer = Xml.newSerializer();
+		try {
+			serializer.setOutput(fileos, "UTF-8");
+			serializer.startDocument(null, Boolean.valueOf(true));
+			serializer.setFeature("http://xmlpull.org/v1/doc/features.html#indent-output", true);
+
+			RadioGroup radioGroupSexe = (RadioGroup) findViewById(R.id.radioGroupSexe);
+	        radioGroupSexe.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+	            public void onCheckedChanged(RadioGroup group, int checkedId) 
+	            {
+	                RadioButton checkedRadioButton = (RadioButton) findViewById(checkedId);
+	                String text = checkedRadioButton.getText().toString();
+	                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+	            }
+	        });
+	        
+			RadioGroup radioGroupEtat = (RadioGroup) findViewById(R.id.radioGroupEtat);
+	        radioGroupEtat.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+	            public void onCheckedChanged(RadioGroup group, int checkedId) 
+	            {
+	                RadioButton checkedRadioButton = (RadioButton) findViewById(checkedId);
+	                String text = checkedRadioButton.getText().toString();
+	                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+	            }
+	        });
+			
+			serializer.startTag(null, "tag");
+			
+				serializer.startTag(null, "sexe");
+				serializer.text(radioGroupSexe.toString());
+				serializer.endTag(null, "sexe");
+           
+				serializer.startTag(null, "age");
+				serializer.text("unknow");
+				serializer.endTag(null, "age");
+   
+				serializer.startTag(null, "etat");
+            	serializer.text(radioGroupEtat.toString());
+            	serializer.endTag(null, "etat");
+            	
+            	serializer.startTag(null, "longitude");
+            	serializer.text("");
+            	serializer.endTag(null, "longitude");
+            	
+            	serializer.startTag(null, "latitude");
+            	serializer.text("");
+            	serializer.endTag(null, "latitude");
+           
+            serializer.endTag(null, "tag");
+            
+            serializer.endDocument();
+            
+            serializer.flush();
+            
+            fileos.close();
+            
+		} catch (Exception e) {
+            Log.e("Exception","error occurred while creating xml file");
+		}
 	}
 	
 	protected String getProvider() {
@@ -106,11 +169,13 @@ public class LocationActivity extends Activity implements OnClickListener, Locat
 		if (bestProvider == null) {
 			Log.i("Localisation message", "Aucun gestionnaire gps trouvé, veuillez activer la localisation");
 			Intent intentMain = new Intent(this, MainActivity.class);
+			createXMLDocument();
 			startActivity(intentMain);
 		} else {
 //			bestProvider = lManager.getBestProvider(criteria, false);
 			lManager.requestLocationUpdates(bestProvider, 5000, 0, this);
 			Intent intentResult = new Intent(this,ResultActivity.class);
+			createXMLDocument();
     		startActivity(intentResult);
 		}
 		return bestProvider;
